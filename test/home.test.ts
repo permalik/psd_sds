a6ea793f02adc5728da31c089fa5877a34dd4ae8
Malik <permalik@protonmail.com>
2022-03-26 20:30:53 -0500

    Fix eslint error

--- test/home.test.ts
+++ test/home.test.ts
@@ -1,8 +1,9 @@
 import home from '../helpers/home';
 import Hero from '../src/components/Hero';
+import Index from '../src/pages';
 
 describe('home test', () => {
 	it('should show home', () => {
-		expect(Hero).toContainHTML('<h1>title of the page</h1>');
+		expect(Hero).toContainHTML('<h1>This is a test!!!</h1>');
 	})
 })

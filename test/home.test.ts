b96dd907dda650c228181f89cf564af5630f3361
Malik <permalik@protonmail.com>
2022-03-28 00:00:28 -0500

    Complete setting up cypress

--- test/home.test.ts
+++
@@ -1,9 +0,0 @@
-import home from '../helpers/home';
-import Hero from '../src/components/Hero';
-import Index from '../src/pages';
-
-describe('home test', () => {
-	it('should show home', () => {
-		expect(Hero).toContainHTML('<h1>This is a test!!!</h1>');
-	})
-})

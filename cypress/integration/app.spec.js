816f42a92d78dfa5b6e7dd17f180c534c297adbf
Malik <permalik@protonmail.com>
2022-03-29 20:19:33 -0500

    Complete Cypress integration tests for SiteHeader nav links

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -16,5 +16,69 @@ describe('Navigation', () => {
 	});
 });
 
+describe('Navigation', () => {
+	it('should navigate to the index page', () => {
+		// Start from the index page
+		cy.visit('http://localhost:3000/');
+
+		// Locate link with href={'/'} and click
+		cy.get('a[href*="#about-section"]').click();
+
+		// The new url should include "/"
+		cy.url().should('include', '#about-section');
+
+		// The new page should contain an h1 with "About page"
+		// 	cy.get('h1').contains('About Page')
+	});
+});
+
+describe('Navigation', () => {
+	it('should navigate to the index page', () => {
+		// Start from the index page
+		cy.visit('http://localhost:3000/');
+
+		// Locate link with href={'/'} and click
+		cy.get('a[href*="#services-section"]').click();
+
+		// The new url should include "/"
+		cy.url().should('include', '#services-section');
+
+		// The new page should contain an h1 with "About page"
+		// 	cy.get('h1').contains('About Page')
+	});
+});
+
+describe('Navigation', () => {
+	it('should navigate to the index page', () => {
+		// Start from the index page
+		cy.visit('http://localhost:3000/');
+
+		// Locate link with href={'/'} and click
+		cy.get('a[href*="#blog-section"]').click();
+
+		// The new url should include "/"
+		cy.url().should('include', '#blog-section');
+
+		// The new page should contain an h1 with "About page"
+		// 	cy.get('h1').contains('About Page')
+	});
+});
+
+describe('Navigation', () => {
+	it('should navigate to the index page', () => {
+		// Start from the index page
+		cy.visit('http://localhost:3000/');
+
+		// Locate link with href={'/'} and click
+		cy.get('a[href*="#contact-section"]').click();
+
+		// The new url should include "/"
+		cy.url().should('include', '#contact-section');
+
+		// The new page should contain an h1 with "About page"
+		// 	cy.get('h1').contains('About Page')
+	});
+});
+
 const asModule = {};
 export default asModule;

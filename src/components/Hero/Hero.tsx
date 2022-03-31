e02ef57c2886afd49dae77a9a759f5b5a21a9277
Malik <permalik@protonmail.com>
2022-03-31 08:25:15 -0500

    Complete Hero component scaffolding

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,11 +1,16 @@
 export default function Hero() {
 	return (
 		<section>
-			<h1>title of the page</h1>
-			<p>subtitle</p>
+			<header>
+				<h1>title of the page</h1>
+				<p>subtitle</p>
+			</header>
 			<p>Varius aonides sed mire promissios genetrix est. Exsuls credere in
 				vierium! Cum advena peregrinationes, omnes abaculuses pugna emeritis,
 				albus nuptiaes.</p>
+			<button>
+				button
+			</button>
 		</section>
 	);
 };

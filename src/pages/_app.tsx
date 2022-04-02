f1df209b5d25dca9a2ba5302da0195dad98336ad
Malik <permalik@protonmail.com>
2022-04-01 20:17:31 -0500

    Implement MUI and Emotion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,42 @@
-import Layout from '../components/Layout';
+import * as React from 'react';
 import type {AppProps} from 'next/app';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+
+import createEmotionCache from '../../utility/createEmotionCache';
+import Layout from '../components/Layout';
 
 import '../styles/globals.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
-function MyApp({Component, pageProps}: AppProps) {
-	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
-	);
+interface MyAppProps extends AppProps {
+	emotionCache?: EmotionCache;
 }
+
+const clientSideEmotionCache = createEmotionCache();
+
+const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
+	const { Component, emotionCache = clientSideEmotionCache, pageProps} = props;
+
+	return (
+		<CacheProvider value={emotionCache}>
+			<CssBaseline/>
+			<Layout>
+				<Component {...pageProps}/>
+			</Layout>
+		</CacheProvider>
+	)
+}
+
+// function MyApp({Component, pageProps}: AppProps) {
+// 	return (
+// 		<Layout>
+// 			<Component {...pageProps} />
+// 		</Layout>
+// 	);
+// }
 
 export default MyApp;

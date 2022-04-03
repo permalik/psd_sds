9d047bad293be78643bf74fc7c4c48453234b89b
Malik <permalik@protonmail.com>
2022-04-02 19:22:30 -0500

    Complete ServiceCard component

---
+++ src/components/ServiceCard/ServiceCard.tsx
@@ -0,0 +1,12 @@
+export default function ServiceCard() {
+	return (
+		<article>
+			<div>
+				<img/>
+			</div>
+			<h2>test</h2>
+			<p>{test}</p>
+			<button>test</button>
+		</article>
+	)
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import Head from 'next/head';
 
 import Hero from '../components/Hero';
+import ServiceCard from '../components/ServiceCard';
 
 import styles from '../styles/Home.module.css';
 
 export default function Index() {
+
 	return (
 		<div className={styles.container}>
 			<Head>
@@ -13,6 +15,7 @@ export default function Index() {
 				<link rel="icon" href="/favicon.ico"/>
 			</Head>
 			<Hero/>
+			<ServiceCard {} />
 		</div>
 	);
 }

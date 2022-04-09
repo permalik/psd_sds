e3f7a98ca42e9f0fe627911ec6fb98f79661d09c
Malik <permalik@protonmail.com>
2022-04-08 22:07:04 -0500

    Create BlogCard component

---
+++ src/components/BlogCard/BlogCard.tsx
@@ -0,0 +1,9 @@
+export default function BlogCard() {
+	return (
+		<article>
+			<h3></h3>
+			<p></p>
+			<button></button>
+		</article>
+	)
+}
--- src/components/BlogSection/BlogSection.tsx
+++ src/components/BlogSection/BlogSection.tsx
@@ -1,7 +1,12 @@
+import BlogCard from '../BlogCard';
+
 export default function BlogSection() {
 	return (
 		<section>
-
+			<h2>from the blog</h2>
+			<BlogCard/>
+			<BlogCard/>
+			<BlogCard/>
 		</section>
 	)
 }

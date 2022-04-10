d994448475f2ebca23fbb50b34e40b1b0422b131
Malik <permalik@protonmail.com>
2022-04-10 08:33:07 -0500

    Complete BlogCard and BlogSection scaffolding

--- src/components/BlogCard/BlogCard.tsx
+++ src/components/BlogCard/BlogCard.tsx
@@ -1,9 +1,13 @@
-export default function BlogCard() {
+export default function BlogCard(props: {blogTitle: any; blogParagraph: any; blogButton: any;}) {
+	const blogTitle = props.blogTitle;
+	const blogParagraph = props.blogParagraph;
+	const blogButton = props.blogButton;
+
 	return (
 		<article>
-			<h3></h3>
-			<p></p>
-			<button></button>
+			<h3>{blogTitle}</h3>
+			<p>{blogParagraph}</p>
+			<button>{blogButton}</button>
 		</article>
 	)
 }
--- src/components/BlogSection/BlogSection.tsx
+++ src/components/BlogSection/BlogSection.tsx
@@ -4,9 +4,21 @@ export default function BlogSection() {
 	return (
 		<section>
 			<h2>from the blog</h2>
-			<BlogCard/>
-			<BlogCard/>
-			<BlogCard/>
+			<BlogCard
+				blogTitle={'title of a post'}
+				blogParagraph={'Varius aonides sed mire promissios genetrix'}
+				blogButton={'button'}
+			/>
+			<BlogCard
+				blogTitle={'title of a post'}
+				blogParagraph={'Varius aonides sed mire promissios genetrix'}
+				blogButton={'button'}
+			/>
+			<BlogCard
+				blogTitle={'title of a post'}
+				blogParagraph={'Varius aonides sed mire promissios genetrix'}
+				blogButton={'button'}
+			/>
 		</section>
 	)
 }

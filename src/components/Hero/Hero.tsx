b24ddd53a9f83f827b805c9da26d5b032ecd0bbe
Malik <permalik@protonmail.com>
2022-04-10 10:02:54 -0500

    Update styles for Hero component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 
+import styles from '../../styles/Hero.module.css'
+
 export default function Hero() {
 	return (
-		<section>
+		<section className={styles.heroSection}>
 			<header>
-				<h1>title of the page</h1>
-				<p>subtitle</p>
+				<h1 className={styles.title}>title of the page</h1>
+				<p className={styles.subtitle}>subtitle</p>
 			</header>
 			<p>Varius aonides sed mire promissios genetrix</p>
 			<Button

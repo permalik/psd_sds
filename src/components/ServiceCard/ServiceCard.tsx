268ec0a373ba7bfd4b4568d90b4ecdd8df2e1b11
Malik <permalik@protonmail.com>
2022-04-10 19:59:16 -0500

    Implement MUI button in ServiceCard component

--- src/components/ServiceCard/ServiceCard.tsx
+++ src/components/ServiceCard/ServiceCard.tsx
@@ -1,3 +1,7 @@
+import Button from '@mui/material/Button';
+import * as React from 'react';
+import { theme } from '../../../theme/mui';
+
 export default function ServiceCard(props: { serviceTitle: any; serviceSummary: any; serviceButton: any; }) {
 	const serviceTitle = props.serviceTitle;
 	const serviceSummary = props.serviceSummary;
@@ -10,7 +14,12 @@ export default function ServiceCard(props: { serviceTitle: any; serviceSummary: 
 			</div>
 			<h3>{serviceTitle}</h3>
 			<p>{serviceSummary}</p>
-			<button>{serviceButton}</button>
+			<Button
+				variant={'contained'}
+				style={{ backgroundColor: theme.palette.secondary.main }}
+			>
+				{serviceButton}
+			</Button>
 		</article>
 	);
 };

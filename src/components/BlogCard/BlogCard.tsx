d6771b82061bf09abb85c7b50d05601f5ba76fed
Malik <permalik@protonmail.com>
2022-04-14 14:40:26 -0500

    Style BlogSection and BlogCard components

--- src/components/BlogCard/BlogCard.tsx
+++ src/components/BlogCard/BlogCard.tsx
@@ -2,21 +2,36 @@ import Button from '@mui/material/Button';
 import * as React from 'react';
 import { theme } from '../../../theme/mui';
 
-export default function BlogCard(props: {blogTitle: any; blogParagraph: any; blogButton: any;}) {
+import styles from '../../styles/BlogCard.module.css';
+
+export default function BlogCard(props: { blogImgAlt: any; blogImg: any; blogTitle: any; blogParagraph: any; blogButton: any; }) {
+	const blogImg = props.blogImg;
+	const blogImgAlt = props.blogImgAlt;
 	const blogTitle = props.blogTitle;
 	const blogParagraph = props.blogParagraph;
 	const blogButton = props.blogButton;
 
 	return (
-		<article>
-			<h3>{blogTitle}</h3>
-			<p>{blogParagraph}</p>
-			<Button
-				style={{ backgroundColor: theme.palette.secondary.main }}
-				variant={'contained'}
-			>
-				{blogButton}
-			</Button>
+		<article className={styles.blogArticle}>
+			<img
+				alt={blogImgAlt}
+				className={styles.blogImg}
+				src={blogImg}
+			/>
+			<div>
+				<h3 className={styles.blogTitle}>{blogTitle}</h3>
+				<p>{blogParagraph}</p>
+				<Button
+					style={{
+						backgroundColor: theme.palette.secondary.main,
+						borderRadius: '25px',
+						padding: '.5rem 2rem',
+					}}
+					variant={'contained'}
+				>
+					{blogButton}
+				</Button>
+			</div>
 		</article>
-	)
+	);
 }
--- src/components/BlogSection/BlogSection.tsx
+++ src/components/BlogSection/BlogSection.tsx
@@ -1,24 +1,34 @@
 import BlogCard from '../BlogCard';
 
+import styles from '../../styles/BlogSection.module.css';
+
 export default function BlogSection() {
 	return (
-		<section>
-			<h2>from the blog</h2>
-			<BlogCard
-				blogTitle={'title of a post'}
-				blogParagraph={'Varius aonides sed mire promissios genetrix'}
-				blogButton={'post1'}
-			/>
-			<BlogCard
-				blogTitle={'title of a post'}
-				blogParagraph={'Varius aonides sed mire promissios genetrix'}
-				blogButton={'post2'}
-			/>
-			<BlogCard
-				blogTitle={'title of a post'}
-				blogParagraph={'Varius aonides sed mire promissios genetrix'}
-				blogButton={'post3'}
-			/>
+		<section className={styles.blogSection}>
+			<div className={styles.container}>
+				<h2 className={styles.blogHeading}>from the blog</h2>
+				<BlogCard
+					blogImgAlt={'post image 1'}
+					blogImg={'https://res.cloudinary.com/permalik/image/upload/v1649964221/sds_blog_img.jpg'}
+					blogTitle={'title of a post'}
+					blogParagraph={'Varius aonides sed mire promissios genetrix'}
+					blogButton={'post1'}
+				/>
+				<BlogCard
+					blogImgAlt={'post image 1'}
+					blogImg={'https://res.cloudinary.com/permalik/image/upload/v1649964221/sds_blog_img.jpg'}
+					blogTitle={'title of a post'}
+					blogParagraph={'Varius aonides sed mire promissios genetrix'}
+					blogButton={'post2'}
+				/>
+				<BlogCard
+					blogImgAlt={'post image 1'}
+					blogImg={'https://res.cloudinary.com/permalik/image/upload/v1649964221/sds_blog_img.jpg'}
+					blogTitle={'title of a post'}
+					blogParagraph={'Varius aonides sed mire promissios genetrix'}
+					blogButton={'post3'}
+				/>
+			</div>
 		</section>
-	)
+	);
 }
